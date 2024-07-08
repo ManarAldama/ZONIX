d6556e8d585ce7af722eadee3279816e2dae4ac3
manar <isc@LAPTOP-M87C5OCJ>
2024-07-08 17:43:26 +0300

    first commit

---
+++ drupal-10.2.7/sites/default/files/php/twig/667732e76a5f3_addtoany-follow.html.twig_utu9QUc2T4rDE5ykKix0NP9LG/lt9cBqKxoNgk9SwOO8PJ0JEfgNGTGKmuouCgxn9jEF8.php
@@ -0,0 +1,121 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/addtoany/templates/addtoany-follow.html.twig */
+class __TwigTemplate_72a856b66209924139168d153c689be8 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 14
+        ob_start(function () { return ''; });
+        // line 15
+        echo "
+  <span class=\"a2a_kit a2a_kit_size_";
+        // line 16
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["buttons_size"] ?? null), 16, $this->source), "html", null, true);
+        echo " a2a_follow addtoany_list\">
+    ";
+        // line 17
+        if (($context["addtoany_html"] ?? null)) {
+            // line 18
+            echo "      ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->sandbox->ensureToStringAllowed(($context["addtoany_html"] ?? null), 18, $this->source));
+            echo "
+    ";
+        }
+        // line 20
+        echo "  </span>
+
+";
+        $___internal_parse_1_ = ('' === $tmp = ob_get_clean()) ? '' : new Markup($tmp, $this->env->getCharset());
+        // line 14
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(twig_spaceless($___internal_parse_1_));
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["buttons_size", "addtoany_html"]);    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "modules/addtoany/templates/addtoany-follow.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  61 => 14,  56 => 20,  50 => 18,  48 => 17,  44 => 16,  41 => 15,  39 => 14,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/addtoany/templates/addtoany-follow.html.twig", "C:\\xampp\\htdocs\\newWebsite\\drupal-10.2.7\\modules\\addtoany\\templates\\addtoany-follow.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("apply" => 14, "if" => 17);
+        static $filters = array("escape" => 16, "raw" => 18, "spaceless" => 14);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['apply', 'if'],
+                ['escape', 'raw', 'spaceless'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
